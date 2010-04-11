b4314148a56382da9cf7dc82fe7dab78258d1253
Severin Smith <severin@smallbutdigital.com>
2010-04-10 21:18:57 -0400

    Ant build file

--- src/compile.sh
+++
@@ -1,8 +0,0 @@
-mkdir ../bin
-javac -d ../bin/ -classpath /Applications/Processing.app/Contents/Resources/Java/core.jar *.java
-cd ../bin/
-jar -cf panoia.jar ./panoia/
-mv panoia.jar ../library
-rm -rf panoia
-cd ..
-rm -rf bin
