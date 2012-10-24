0926dd0be89f5da09ea1910d440aa195fd0befe2
Severin Smith <info@smallbutdigital.com>
2012-10-24 02:46:13 -0400

    Now working nicely with links and wrapping

--- examples/PanoramaTest/PanoramaTest.java
+++ examples/PanoramaTest/PanoramaTest.java
@@ -5,100 +5,62 @@ public class PanoramaTest extends PApplet {
 
 	Pano pano;
 	float angleOffset;
-	PanoTileData tiles;
-	PImage[][] pics;
+	PanoLink[] links;
+
+	float fov = 270;
+
 	public void setup() {
-		size(displayWidth, (int)(displayWidth/6.5*3));
+		size(displayWidth, (int)(displayWidth/(6.5f*360/360)));
 
 		background(0);
 		
-		pano = new Pano();
+		pano = new Pano(this);
 		pano.setPosition(new LatLng(45.537734f, -73.622578f));
 
-
-		tiles = pano.getPanoData().tiles;
-		
-		pics = new PImage[7][3];
-		
-		for(int i = 0;i < 7;i++) {
-			for(int j = 0;j < 3;j++) {
-				pics[i][j] = loadImage(tiles.getTileUrl(3,i,j), "jpg");
-			}
-		}
+		links = pano.getLinks();
 	}
 
 	public void draw() {
 		background(0);
+
 		pushMatrix();
 		translate(width/2, 0);
-
-		float centerHeading = angleOffset;
-		float xPov = 180;
-		int tileSize = (int)(width/6.5f);
-
-		int xTiles = 7;
-		int yTiles = 3;
-		float wrapTile = 6.5f;
-		float carTile = 3.25f;
-
-		float targetHeading = mod(tiles.centerHeading-centerHeading, 360);
-
-		float centerTile = mod(carTile-targetHeading/360*wrapTile, wrapTile);
-		float povTileLen = xPov/360*wrapTile;
-		float startTile = mod(centerTile-povTileLen/2, wrapTile);
-
-		float offsetX = (mod(startTile,1)+povTileLen/2)*tileSize;
-
-		int startIndex = floor(startTile);	
-
-		for(int i = 0;i <= ceil(povTileLen);i++) {
-			for(int j = 1;j < 2;j++) {
-				if(pics[(i+startIndex)%xTiles][j] != null) {
-					if(i+startIndex < 7) {
-						image(pics[(i+startIndex)%7][j], tileSize*i-offsetX, tileSize*j, tileSize, tileSize);
-					} else {
-						image(pics[(i+startIndex)%7][j], tileSize*(i-0.5f)-offsetX, tileSize*j, tileSize, tileSize);
-					}
-				}
-			}
-		}
+		pano.drawTiles(-angleOffset, 360, displayWidth);
+		stroke(255);
+		line(-width/2, 0, -width/2, height);
+		line(width/2, 0, width/2, height);
+		popMatrix();
 
 		angleOffset = map(mouseX, 0, width, -180, 180);
-		popMatrix();
-		stroke(255);
-		strokeWeight(5);
-		line(width/4, 0, width/4, height);
-		line(3*width/4, 0, 3*width/4, height);
 
+		stroke(255);
+		for(int i = 0;i < links.length;i++) {
+			float heading = links[i].heading;
+			if(heading > 180) heading -= 360;
+			int xpos = (int)((3.25f+(heading+angleOffset)/360*6.5f)*displayWidth/6.5f);
+			line(xpos, 0, xpos, height);
+		}
+
+		stroke(255, 0, 0);
+		line(width/2, 0, width/2, height);
 	}
 
-	float mod(float v, float max) {
-		if(v == max) return 0;
-		while(v < 0) v += max;
-		while(v > max) v -= max;
-		return v;
+	public void keyPressed() {
+		if(key == 'n') {
+			PanoLink closest = links[0];
+			int thead = (int)(-angleOffset+360)%360;
+			for(int i = 0;i < links.length;i++) {
+				if(abs(closest.heading-thead) > abs(links[i].heading-thead)) {
+					closest = links[i];
+				}
+			}
+
+			pano.setPano(closest.pano);
+			links = pano.getLinks();
+		}
 	}
 
 	public static void main (String [] args) {
 		PApplet.main(new String[] { "PanoramaTest" });
 	}
-
-	public void mousePressed() {
-		System.out.println(mouseX*6.5f/width);
-	}
-
-	public void keyPressed() {
-		if(key == 'n') {
-			pano.setPano(pano.getLinks()[0].pano);
-
-			tiles = pano.getPanoData().tiles;
-
-			for(int i = 0;i < 7;i++) {
-				for(int j = 0;j < 3;j++) {
-					pics[i][j] = loadImage(tiles.getTileUrl(3,i,j), "jpg");
-				}
-			}
-		}
-	}
-
 }
--- src/panoia/Pano.java
+++ src/panoia/Pano.java
@@ -1,6 +1,6 @@
 package panoia;
 
-import processing.core.*
+import processing.core.*;
 
 public class Pano {
 
@@ -9,11 +9,16 @@ public class Pano {
 	PanoProvider provider;
 
 	PanoData data;
+	PanoPov pov;
+
+	PImage[][] tileCache;
 
 	public Pano(PApplet p) {
 		this.p = p;
 		this.provider = new PanoProvider();
 		pov = new PanoPov();
+
+		tileCache = new PImage[7][3];
 	}
 
 	public PanoLink[] getLinks() {
@@ -34,23 +39,70 @@ public class Pano {
 
 	public void setPano(String pano) {
 		data = provider.getPanoData(pano);
+		for(int i = 0;i < tileCache.length;i++) {
+			for(int j = 0;j < tileCache[i].length;j++) {
+				tileCache[i][j] = null;
+			}
+		}
 	}
 
 	public void setPosition(LatLng latLng) {
 		data = provider.getPanoData(latLng);
+		for(int i = 0;i < tileCache.length;i++) {
+			for(int j = 0;j < tileCache[i].length;j++) {
+				tileCache[i][j] = null;
+			}
+		}
 	}
 
 	public void setPov(PanoPov pov) {
 		this.pov = pov;
-		//Adjust field of view??
 	}
 
 	public PanoData getPanoData() {
 		return data;
 	}
 
-	public PImage getStaticView(PanoPov pov, float fov) {
+	public void drawTiles(float heading, float fov, int imageWidth) {
+		int xTiles = 7;
+		int yTiles = 3;
+		float overlap = 0.5f;
+		float wrapTile = 6.5f;
+		float carTile = 3.25f;
+		int tileSize = (int)(imageWidth/(wrapTile*fov/360));
 
+		float targetHeading = mod(data.centerHeading-heading, 360);
+
+		float centerTile = mod(carTile-targetHeading/360*wrapTile, wrapTile);
+		float povTileLen = fov/360*wrapTile;
+		float startTile = mod(centerTile-povTileLen/2, wrapTile);
+
+		float offsetX = (mod(startTile,1)+povTileLen/2)*tileSize;
+
+		int startIndex = p.floor(startTile);	
+
+		for(int i = 0;i <= p.ceil(povTileLen);i++) {
+			// for(int j = 0;j < yTiles;j++) {
+			for(int j = 1;j < 2;j++) {
+				if(tileCache[(i+startIndex)%xTiles][j] == null) {
+					tileCache[(i+startIndex)%xTiles][j] = p.loadImage(data.getTileUrl(3,(i+startIndex)%xTiles,j), "jpg");
+				}
+				if(i+startIndex < xTiles) {
+					p.image(tileCache[(i+startIndex)%xTiles][j], tileSize*i-offsetX, 0, tileSize, tileSize);
+					// p.image(tileCache[(i+startIndex)%xTiles][j], tileSize*i-offsetX, tileSize*j, tileSize, tileSize);
+				} else {
+					p.image(tileCache[(i+startIndex)%xTiles][j], tileSize*(i-overlap)-offsetX, 0, tileSize, tileSize);
+					// p.image(tileCache[(i+startIndex)%xTiles][j], tileSize*(i-overlap)-offsetX, tileSize*j, tileSize, tileSize);
+				}
+			}
+		}
+	}
+
+	float mod(float v, float max) {
+		if(v == max) return 0;
+		while(v < 0) v += max;
+		while(v > max) v -= max;
+		return v;
 	}
 
 }
