37ec523817d91dcaf273495004a1e10d9f2c9ea9
Severin Smith <info@smallbutdigital.com>
2012-11-01 10:38:11 -0400

    Null pointers

--- src/panoia/Pano.java
+++ src/panoia/Pano.java
@@ -113,51 +113,59 @@ public class Pano {
 
 	public void drawThreeFold(int imageWidth) {
 		if(data == null) return;
-		for(int i = 0;i < 3;i++) {
-			if(threeFoldCache[i] == null) {
-				threeFoldCache[i] = p.loadImage(data.getStaticUrl(640, 480, 90*(i-1)+pov.heading(), 0, 90), "jpg");
+		try {
+			for(int i = 0;i < 3;i++) {
+				if(threeFoldCache[i] == null) {
+					threeFoldCache[i] = p.loadImage(data.getStaticUrl(640, 480, 90*(i-1)+pov.heading(), 0, 90), "jpg");
+				}
+				if(threeFoldCache[i] != null) {
+					p.image(threeFoldCache[i], imageWidth/3*(i-1)-imageWidth/6, 0, imageWidth/3, imageWidth*480/3/640);
+				}
 			}
-			if(threeFoldCache[i] != null) {
-				p.image(threeFoldCache[i], imageWidth/3*(i-1)-imageWidth/6, 0, imageWidth/3, imageWidth*480/3/640);
-			}
+		} catch(Exception e) {
+
 		}
 	}
 
 	public void drawTiles(float fov, int imageWidth) {
 		if(data == null) return;
-		int xTiles = 7;
-		int yTiles = 3;
-		float overlap = 0.5f;
-		float wrapTile = 6.5f;
-		float carTile = 3.25f;
-		int tileSize = (int)(imageWidth/(wrapTile*fov/360));
+		try {
+			int xTiles = 7;
+			int yTiles = 3;
+			float overlap = 0.5f;
+			float wrapTile = 6.5f;
+			float carTile = 3.25f;
+			int tileSize = (int)(imageWidth/(wrapTile*fov/360));
 
-		float targetHeading = mod(data.centerHeading-pov.heading(), 360);
+			float targetHeading = mod(data.centerHeading-pov.heading(), 360);
 
-		float centerTile = mod(carTile-targetHeading/360*wrapTile, wrapTile);
-		float povTileLen = fov/360*wrapTile;
-		float startTile = mod(centerTile-povTileLen/2, wrapTile);
+			float centerTile = mod(carTile-targetHeading/360*wrapTile, wrapTile);
+			float povTileLen = fov/360*wrapTile;
+			float startTile = mod(centerTile-povTileLen/2, wrapTile);
 
-		float offsetX = (mod(startTile,1)+povTileLen/2)*tileSize;
+			float offsetX = (mod(startTile,1)+povTileLen/2)*tileSize;
 
-		int startIndex = p.floor(startTile);	
+			int startIndex = p.floor(startTile);	
 
-		for(int i = 0;i <= p.ceil(povTileLen)+1;i++) {
-			// for(int j = 0;j < yTiles;j++) {
-			for(int j = 1;j < 2;j++) {
-				if(tileCache[(i+startIndex)%xTiles][j] == null) {
-					tileCache[(i+startIndex)%xTiles][j] = p.loadImage(data.getTileUrl(3,(i+startIndex)%xTiles,j), "jpg");
-				}
-				if(tileCache[(i+startIndex)%xTiles][j] != null) {
-					if(i+startIndex < xTiles) {
-						p.image(tileCache[(i+startIndex)%xTiles][j], tileSize*i-offsetX, 0, tileSize, tileSize);
-						// p.image(tileCache[(i+startIndex)%xTiles][j], tileSize*i-offsetX, tileSize*j, tileSize, tileSize);
-					} else {
-						p.image(tileCache[(i+startIndex)%xTiles][j], tileSize*(i-overlap)-offsetX, 0, tileSize, tileSize);
-						// p.image(tileCache[(i+startIndex)%xTiles][j], tileSize*(i-overlap)-offsetX, tileSize*j, tileSize, tileSize);
+			for(int i = 0;i <= p.ceil(povTileLen)+1;i++) {
+				// for(int j = 0;j < yTiles;j++) {
+				for(int j = 1;j < 2;j++) {
+					if(tileCache[(i+startIndex)%xTiles][j] == null) {
+						tileCache[(i+startIndex)%xTiles][j] = p.loadImage(data.getTileUrl(3,(i+startIndex)%xTiles,j), "jpg");
+					}
+					if(tileCache[(i+startIndex)%xTiles][j] != null) {
+						if(i+startIndex < xTiles) {
+							p.image(tileCache[(i+startIndex)%xTiles][j], tileSize*i-offsetX, 0, tileSize, tileSize);
+							// p.image(tileCache[(i+startIndex)%xTiles][j], tileSize*i-offsetX, tileSize*j, tileSize, tileSize);
+						} else {
+							p.image(tileCache[(i+startIndex)%xTiles][j], tileSize*(i-overlap)-offsetX, 0, tileSize, tileSize);
+							// p.image(tileCache[(i+startIndex)%xTiles][j], tileSize*(i-overlap)-offsetX, tileSize*j, tileSize, tileSize);
+						}
 					}
 				}
 			}
+		} catch(Exception e) {
+
 		}
 	}
 
