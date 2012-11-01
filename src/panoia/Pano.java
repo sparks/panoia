3e52eaf25d4f790d1dd3633ee9f32705fd99895f
Severin Smith <info@smallbutdigital.com>
2012-11-01 10:35:58 -0400

    Updated to fix null pointers

--- src/panoia/Pano.java
+++ src/panoia/Pano.java
@@ -32,19 +32,23 @@ public class Pano {
 	}
 
 	public PanoLink[] getLinks() {
-		return data.links;
+		if(data != null) return data.links;
+		else return new PanoLink[0];
 	}
 
 	public String getPano() {
-		return data.location.pano;
+		if(data != null) return data.location.pano;
+		else return "";
 	}
 
 	public LatLng getPosition() {
-		return data.location.latLng;
+		if(data != null) return data.location.latLng;
+		else return new LatLng(0, 0);
 	}
 
 	public PanoPov getPov() {
-		return pov;
+		if(data != null) return pov;
+		else return new PanoPov(0, 0, 0);
 	}
 
 	public void setPano(String pano) {
@@ -88,6 +92,7 @@ public class Pano {
 	}
 
 	public void jump() {
+		if(data == null) return;
 		PanoLink closest = data.links[0];
 		for(int i = 0;i < data.links.length;i++) {
 			if(p.abs(closest.heading-pov.heading()) > p.abs(data.links[i].heading-pov.heading())) {
@@ -107,58 +112,52 @@ public class Pano {
 	}
 
 	public void drawThreeFold(int imageWidth) {
-		try {
-			for(int i = 0;i < 3;i++) {
-				if(threeFoldCache[i] == null) {
-					threeFoldCache[i] = p.loadImage(data.getStaticUrl(640, 480, 90*(i-1)+pov.heading(), 0, 90), "jpg");
-				}
-				if(threeFoldCache[i] != null) {
-					p.image(threeFoldCache[i], imageWidth/3*(i-1)-imageWidth/6, 0, imageWidth/3, imageWidth*480/3/640);
-				}
+		if(data == null) return;
+		for(int i = 0;i < 3;i++) {
+			if(threeFoldCache[i] == null) {
+				threeFoldCache[i] = p.loadImage(data.getStaticUrl(640, 480, 90*(i-1)+pov.heading(), 0, 90), "jpg");
 			}
-		} catch(Exception e) {
-
+			if(threeFoldCache[i] != null) {
+				p.image(threeFoldCache[i], imageWidth/3*(i-1)-imageWidth/6, 0, imageWidth/3, imageWidth*480/3/640);
+			}
 		}
 	}
 
 	public void drawTiles(float fov, int imageWidth) {
-		try {
-			int xTiles = 7;
-			int yTiles = 3;
-			float overlap = 0.5f;
-			float wrapTile = 6.5f;
-			float carTile = 3.25f;
-			int tileSize = (int)(imageWidth/(wrapTile*fov/360));
+		if(data == null) return;
+		int xTiles = 7;
+		int yTiles = 3;
+		float overlap = 0.5f;
+		float wrapTile = 6.5f;
+		float carTile = 3.25f;
+		int tileSize = (int)(imageWidth/(wrapTile*fov/360));
 
-			float targetHeading = mod(data.centerHeading-pov.heading(), 360);
+		float targetHeading = mod(data.centerHeading-pov.heading(), 360);
 
-			float centerTile = mod(carTile-targetHeading/360*wrapTile, wrapTile);
-			float povTileLen = fov/360*wrapTile;
-			float startTile = mod(centerTile-povTileLen/2, wrapTile);
+		float centerTile = mod(carTile-targetHeading/360*wrapTile, wrapTile);
+		float povTileLen = fov/360*wrapTile;
+		float startTile = mod(centerTile-povTileLen/2, wrapTile);
 
-			float offsetX = (mod(startTile,1)+povTileLen/2)*tileSize;
+		float offsetX = (mod(startTile,1)+povTileLen/2)*tileSize;
 
-			int startIndex = p.floor(startTile);	
+		int startIndex = p.floor(startTile);	
 
-			for(int i = 0;i <= p.ceil(povTileLen)+1;i++) {
-				// for(int j = 0;j < yTiles;j++) {
-				for(int j = 1;j < 2;j++) {
-					if(tileCache[(i+startIndex)%xTiles][j] == null) {
-						tileCache[(i+startIndex)%xTiles][j] = p.loadImage(data.getTileUrl(3,(i+startIndex)%xTiles,j), "jpg");
-					}
-					if(tileCache[(i+startIndex)%xTiles][j] != null) {
-						if(i+startIndex < xTiles) {
-							p.image(tileCache[(i+startIndex)%xTiles][j], tileSize*i-offsetX, 0, tileSize, tileSize);
-							// p.image(tileCache[(i+startIndex)%xTiles][j], tileSize*i-offsetX, tileSize*j, tileSize, tileSize);
-						} else {
-							p.image(tileCache[(i+startIndex)%xTiles][j], tileSize*(i-overlap)-offsetX, 0, tileSize, tileSize);
-							// p.image(tileCache[(i+startIndex)%xTiles][j], tileSize*(i-overlap)-offsetX, tileSize*j, tileSize, tileSize);
-						}
+		for(int i = 0;i <= p.ceil(povTileLen)+1;i++) {
+			// for(int j = 0;j < yTiles;j++) {
+			for(int j = 1;j < 2;j++) {
+				if(tileCache[(i+startIndex)%xTiles][j] == null) {
+					tileCache[(i+startIndex)%xTiles][j] = p.loadImage(data.getTileUrl(3,(i+startIndex)%xTiles,j), "jpg");
+				}
+				if(tileCache[(i+startIndex)%xTiles][j] != null) {
+					if(i+startIndex < xTiles) {
+						p.image(tileCache[(i+startIndex)%xTiles][j], tileSize*i-offsetX, 0, tileSize, tileSize);
+						// p.image(tileCache[(i+startIndex)%xTiles][j], tileSize*i-offsetX, tileSize*j, tileSize, tileSize);
+					} else {
+						p.image(tileCache[(i+startIndex)%xTiles][j], tileSize*(i-overlap)-offsetX, 0, tileSize, tileSize);
+						// p.image(tileCache[(i+startIndex)%xTiles][j], tileSize*(i-overlap)-offsetX, tileSize*j, tileSize, tileSize);
 					}
 				}
 			}
-		} catch(Exception e) {
-
 		}
 	}
 
